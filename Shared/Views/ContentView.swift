f52652b38009ad6a286774ba17397ec2081059cc
Umayanga Alahakoon <umayanga@usa.com>
2022-03-21 17:12:19 +0530

    Added a menu with relevant links

--- Shared/Views/ContentView.swift
+++ Shared/Views/ContentView.swift
@@ -9,20 +9,30 @@ import SwiftUI
 
 struct ContentView: View {
     @StateObject var coinVM = CoinVM()
-    @AppStorage("trackingSymbol") var trackingSymbol = "APEUSDT"
+    // ape coin symbol on binance
+    var trackingSymbol = "APEUSDT"
     
+    // Links
+    let apeCoinWebsite = URL(string: "https://apecoin.com")!
+    let apeCoinContract = URL(string: "https://etherscan.io/token/0x4d224452801aced8b2f0aebe155379bb5d594381")!
+    let appSourceCode = URL(string: "https://github.com/USBA/ApeTracker")!
+    let binanceAPI = URL(string: "https://binance-docs.github.io/apidocs/spot/en/#symbol-price-ticker")!
+    let developerTwitter = URL(string: "https://twitter.com/metaUSB")!
     
     var body: some View {
         ZStack {
-            
+            // background image
             Image("ApeCoin_Background")
                 .resizable()
                 .aspectRatio(contentMode: .fill)
-                .clipped()
                 .blur(radius: 50)
                 .ignoresSafeArea(.all)
+                .clipped()
             
             VStack(spacing: 0) {
+                Spacer()
+                
+                // ape coin logo
                 Image("ApeCoin_Logo")
                     .resizable()
                     .aspectRatio(contentMode: .fit)
@@ -33,21 +43,29 @@ struct ContentView: View {
                     }
                     .padding(.bottom, 60)
                 
+                // ape coin price
                 Text(Double(coinVM.coinPrice)?.formatted(.number) ?? "0.0000")
                     .font(.system(size: 60, design: .default))
                     .fontWeight(.bold)
                     .lineLimit(1)
                     .redacted(reason: coinVM.coinPrice.isEmpty ? .placeholder : [])
                 
+                // USD currency text
                 Text("USD")
                     .font(.system(size: 30, design: .default))
                     .fontWeight(.semibold)
                     .lineLimit(1)
                     .foregroundColor(.white.opacity(0.75))
                     .redacted(reason: coinVM.coinPrice.isEmpty ? .placeholder : [])
+                
+                Spacer()
+                
+                bottomMenu
+
             }
             .foregroundColor(.white)
-            .padding(35)
+            .padding(.horizontal, 35)
+            .padding(.vertical, 20)
         }
         .preferredColorScheme(.dark)
         .task {
@@ -55,6 +73,37 @@ struct ContentView: View {
         }
     }
     
+    // bottom menu with relevant links
+    var bottomMenu: some View {
+        Menu {
+            Link(destination: developerTwitter) {
+                Label("Developer", systemImage: "person")
+            }
+            Divider()
+            Link(destination: binanceAPI) {
+                Label("Binance Open API", systemImage: "antenna.radiowaves.left.and.right")
+            }
+            Link(destination: appSourceCode) {
+                Label("Source Code", systemImage: "chevron.left.forwardslash.chevron.right")
+            }
+            Divider()
+            Link(destination: apeCoinContract) {
+                Label("ApeCoin Contract", systemImage: "scroll")
+            }
+            Link(destination: apeCoinWebsite) {
+                Label("ApeCoin Website", systemImage: "network")
+            }
+        } label: {
+            Image(systemName: "ellipsis")
+                .imageScale(.large)
+                .foregroundColor(.white)
+                .padding(5)
+                .background(Color.white.opacity(0.25))
+                .clipShape(Capsule())
+        }
+    }
+    
+    // get current price api call
     func refreshCoinPrice() {
         if !coinVM.isLoading {
             // vibrate
